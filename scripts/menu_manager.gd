96c52dcc257604e4c4c757f00261c5e9298df3f3
Antoine L. <antoine.lestrade@he-arc.ch>
2025-06-28 23:32:18 +0200

    Enhance menu manager with logo animations and new exit button

--- scripts/menu_manager.gd
+++ scripts/menu_manager.gd
@@ -1,8 +1,13 @@
 extends Control
 
+@onready var popupContainer: VBoxContainer = $VBoxContainer
 @onready var appStore = $AppStore
 @onready var loading_icon = $AppStore/TextureRect/TextureRect2
 @onready var flashScreen = $FlashScreen
+
+@onready var godot_logo: TextureRect = $FlashScreen/CenterContainer/GodotLogo
+@onready var jam_logo: TextureRect = $FlashScreen/CenterContainer/JamLogo
+
 
 func _ready() -> void:
 	appStore.visible = false
@@ -25,8 +30,8 @@ func on_get_pressed() -> void:
 	$AppStore/TextureRect/TextureRect.visible = true
 	$AppStore/TextureRect/Loading.visible = true
 	await get_tree().create_timer(2).timeout
-	$AppStore/TextureRect/TextureRect.visible = true
-	$AppStore/TextureRect/Loading.visible = true
+	$AppStore/TextureRect/TextureRect.visible = false
+	$AppStore/TextureRect/Loading.visible = false
 	$AppStore/TextureRect/Open.visible = true
 
 func on_open_pressed() -> void:
@@ -36,6 +41,26 @@ func on_open_pressed() -> void:
 
 	flashScreen.visible = true
 	flashScreen.position.y = start_y  # start off-screen
+	
+	# Définir la valeur initiale de modulate (complètement visible)
+	godot_logo.modulate = Color(1, 1, 1, 0)
+	jam_logo.modulate = Color(1, 1, 1, 0)
 
 	var tween = create_tween()
 	tween.tween_property(flashScreen, "position:y", target_y, 0.4).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+
+	tween = create_tween()
+	tween.tween_property(godot_logo, "modulate:a", 1, 1).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	tween.chain().tween_property(godot_logo, "modulate:a", 0, 1).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	tween.chain().tween_property(jam_logo, "modulate:a", 1, 1).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	tween.chain().tween_property(jam_logo, "modulate:a", 0, 1).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+
+
+func _on_fake_exit_button_pressed() -> void:
+	var tween = create_tween()
+	tween.tween_property(popupContainer, "scale:x", 0, 0.4).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	tween.parallel().tween_property(popupContainer, "scale:y", 0, 0.4).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	#await get_tree().create_timer(2).timeout
+	tween.chain().tween_interval(1)
+	tween.chain().tween_property(popupContainer, "scale:x", 1, 0.4).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	tween.parallel().tween_property(popupContainer, "scale:y", 1, 0.4).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
