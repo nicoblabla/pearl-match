90de7cc4d1bf7416dcffb2fb74ff74ab2d31ba80
Nicolas <nicolas.laoun@gmail.com>
2025-06-28 17:48:14 +0200

    UI OVER

--- scripts/menu_manager.gd
+++ scripts/menu_manager.gd
@@ -2,6 +2,11 @@ extends Control
 
 @onready var appStore = $AppStore
 @onready var loading_icon = $AppStore/TextureRect/TextureRect2
+@onready var flashScreen = $FlashScreen
+
+func _ready() -> void:
+	appStore.visible = false
+	flashScreen.visible = false
 
 
 func app_store_pressed() -> void:
@@ -15,16 +20,22 @@ func app_store_pressed() -> void:
 	var tween = create_tween()
 	tween.tween_property(appStore, "position:y", target_y, 0.7).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
 
-
 func on_get_pressed() -> void:
 	$AppStore/TextureRect/ButtonGet.visible = false
 	$AppStore/TextureRect/TextureRect.visible = true
 	$AppStore/TextureRect/Loading.visible = true
 	await get_tree().create_timer(2).timeout
-	show_open()
-
-
-func show_open():
 	$AppStore/TextureRect/TextureRect.visible = true
 	$AppStore/TextureRect/Loading.visible = true
 	$AppStore/TextureRect/Open.visible = true
+
+func on_open_pressed() -> void:
+	var screen_height = get_viewport_rect().size.y
+	var target_y = screen_height - flashScreen.size.y  # final Y position
+	var start_y = screen_height  # off-screen
+
+	flashScreen.visible = true
+	flashScreen.position.y = start_y  # start off-screen
+
+	var tween = create_tween()
+	tween.tween_property(flashScreen, "position:y", target_y, 0.7).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
